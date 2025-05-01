91cbbb282ae9328a49c5e95068df453efa19c598
krishna3052002 <shukrishna23@gmail.com>
2025-05-01 21:31:17 +0600

    Fifth Commit

--- star_cinema_app/views.py
+++ star_cinema_app/views.py
@@ -2,18 +2,28 @@ from django.db import connection
 from django.shortcuts import render
 
 def home(request):
+    theater_id = request.GET.get('theater_id')
+
     with connection.cursor() as cursor:
-        # Fetch all theaters
         cursor.execute("SELECT id, name FROM star_cinema_app_theater")
         theaters = cursor.fetchall()
 
-        # Fetch movies with show details
-        cursor.execute("""
-            SELECT m.id, m.title, m.genre, m.poster_image, t.name AS theater_name, s.show_time 
-            FROM star_cinema_app_movie m
-            JOIN star_cinema_app_show_table s ON m.id = s.movie_id
-            JOIN star_cinema_app_theater t ON t.id = s.theater_id
-        """)
+        if theater_id:
+            cursor.execute("""
+                SELECT m.id, m.title, m.genre, m.poster_image, t.name AS theater_name, s.show_time 
+                FROM star_cinema_app_movie m
+                JOIN star_cinema_app_show_table s ON m.id = s.movie_id
+                JOIN star_cinema_app_theater t ON t.id = s.theater_id
+                WHERE t.id = %s
+            """, [theater_id])
+        else:
+            cursor.execute("""
+                SELECT m.id, m.title, m.genre, m.poster_image, t.name AS theater_name, s.show_time 
+                FROM star_cinema_app_movie m
+                JOIN star_cinema_app_show_table s ON m.id = s.movie_id
+                JOIN star_cinema_app_theater t ON t.id = s.theater_id
+            """)
+        
         movies = cursor.fetchall()
 
     context = {
