6c70d6939180cb9bcc503a9ac8c04dc2949964a8
krishna3052002 <shukrishna23@gmail.com>
2025-05-01 12:57:42 +0600

    Second commit

--- star_cinema_app/views.py
+++ star_cinema_app/views.py
@@ -1,3 +1,23 @@
+from django.db import connection
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    query = request.GET.get('search', '')
+
+    with connection.cursor() as cursor:
+        if query:
+            cursor.execute("SELECT * FROM Movie WHERE Title LIKE %s", ['%' + query + '%'])
+        else:
+            cursor.execute("SELECT * FROM Movie")
+        movie_rows = cursor.fetchall()
+
+        cursor.execute("SELECT * FROM Theater")
+        theater_rows = cursor.fetchall()
+
+    # Convert raw SQL rows to dictionaries (optional, for easier template use)
+    movies = [{'id': row[0], 'title': row[1], 'genre': row[2], 'duration': row[3],
+               'description': row[4], 'poster_image': row[5]} for row in movie_rows]
+
+    theaters = [{'id': row[0], 'name': row[1], 'location': row[2], 'capacity': row[3]} for row in theater_rows]
+
+    return render(request, 'star_cinema_app/index.html', {'movies': movies, 'theaters': theaters})
